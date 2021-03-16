3bcefcc2acb8db2ea329f6b66337c4998cc79ce1
andreabecerrab <andii1wiki@gmail.com>
2021-03-15 21:04:02 -0600

    change pink color

--- resources/views/splendid/create.blade.php
+++ resources/views/splendid/create.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>hola</h1>
+<div class="card mt-5 light-green ">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
 @endsection
