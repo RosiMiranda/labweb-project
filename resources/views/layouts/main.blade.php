34e05b0b3259c81919852d21af9edc2ad6dd8ab4
andreabecerrab <andii1wiki@gmail.com>
2021-03-17 13:30:15 -0600

    fix buttons for responsive menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -38,7 +38,8 @@
                 @endif
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon material-icons">table_rows</span>
+
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
