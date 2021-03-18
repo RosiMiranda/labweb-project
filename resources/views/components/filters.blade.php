b77dec67586e323e1c92361b013808515e7baaea
rosmir <36215317+RosiMiranda@users.noreply.github.com>
2021-03-18 12:43:20 -0600

    Category & Filter section creation
    Category component created.
    The category is composed of Filters (Another component, with hard data until now)

---
+++ resources/views/components/filters.blade.php
@@ -0,0 +1,6 @@
+<h3 class="title">Category</h3>
+        <div class="row">
+                        <x-category title="Vestidos"></x-category>
+                        <x-category title="Playeras"></x-category>
+                        <x-category title="Pantalones"></x-category>
+        </div>
--- resources/views/splendid/store.blade.php
+++ resources/views/splendid/store.blade.php
@@ -5,4 +5,8 @@
 <div class="container arrows" style="width: 50%;">
     <h4 style="opacity:0%;">hola</h4>
 </div>
+<div class='container'>
+        <x-filters></x-filters>
+</div>
+    
 @endsection
