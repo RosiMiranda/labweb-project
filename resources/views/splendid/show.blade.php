75b8191eacbca9e81d9b5ec9b8a4b0eb30ea262e
rosmir <36215317+RosiMiranda@users.noreply.github.com>
2021-03-18 22:08:21 -0600

    Product view
    View with hard data

--- resources/views/splendid/show.blade.php
+++ resources/views/splendid/show.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>Here goes product</h1>
+    <div class="divProductBackground">
+        <div class="row">
+            <div class="column">
+                <h3>Descripcion prenda</h3>
+                <h3>categoria</h3>
+                <h3>description categoria</h3>
+                <h3>Talla</h3>
+            </div>
+            <div class="column">
+                <img src="{{ url('img/jersey.png') }}" alt="...">
+                <h1 style="margin-right: 2em;">precio</h1>
+            </div>
+        </div>
+    </div>
 @endsection
