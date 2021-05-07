a4a6e7c127c0b2244569e2c9f99c13da69a8a617
andreabecerrab <andii1wiki@gmail.com>
2021-05-07 11:59:56 -0500

    add price in products

--- resources/views/splendid/single-product.blade.php
+++ resources/views/splendid/single-product.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
 <div class="container align-center mt-3" style="min-height:80vh;">
-   
+
     <div class="row div-product ">
         <div class="col-12 col-sm-6 col-product-info">
-           
+
             <h1>{{$product->description}}
                 <a  href="{{ route('splendid.editProduct', ['product' => $product]) }}">
                     <img class="editProduct " src="{{ url('img/edit.png') }}" alt="..." >
                 </a>
             </h1>
-            <h2>{{$product->description}}</h2>
+            <h2>${{$product->price}}</h2>
             <h3>Talla: {{$product->size}}</h3>
             <h2>Categorias:</h2>
             <div class="row">
@@ -22,7 +22,7 @@
             <button class="button primary mt-3">Agregar al carrito</button>
         </div>
         <div class="col-12 col-sm-6  col-product-info">
-            
+
             <img src="{{ url('img/ropa1.png') }}" alt="..." class="col-product-img">
         </div>
 
