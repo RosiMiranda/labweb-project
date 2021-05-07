ca62284da9004313eb4b0d67ab925684f5241c6a
liaDiaz <46771468+liaDiaz@users.noreply.github.com>
2021-05-07 02:22:13 -0500

    product

---
+++ resources/views/splendid/edit-product.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="container">
+    <h1 class="title">Nueva Prenda</h1>
+    <div class="container arrows" style="width: 50%;">
+        <h4 style="opacity:0%;">hola</h4>
+    </div>
+    <div class="card mt-2 light-green">
+        <div class="card-body">
+            <form action="{{ route('splendid.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label style="margin-top:2rem;">Select category(ies) for this product</label></br>
+                            @foreach ($categories as $item)
+                            <input type="checkbox" name="categories[]" value="{{ $item->id }}">
+                            <label for="{{ $item->id }}" > {{ $item->name }}</label><br>
+                            @endforeach
+                        </div>
+                        <div class="mb-3">
+                            <label  class="form-label">Descripción</label>
+                            <input type="text" class=" green-inputs dark-green " name="description" >
+                        </div>
+                        <div class="mb-3">
+                            <label  class="form-label">Precio</label>
+                            <input type="text" class="dark-green green-inputs" name="price">
+                        </div>
+                        <div class="mb-3">
+                            <label  class="form-label">Talla</label>
+                            <input type="text" class="dark-green green-inputs" name="size">
+                        </div>
+                        <input type="submit" value="Submit" class="button primary">
+                    </div>
+                    <div class="col-6">
+                        <div class="button-wrapper">
+                            <span class="label">
+                                <span class="material-icons" style="font-size: 100px !important">
+                                    photo_camera
+                                </span>
+                            </span>
+                            <input type="file" id="upload" class="upload-box" name="file">
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+<div>
+@endsection
+
--- resources/views/splendid/single-product.blade.php
+++ resources/views/splendid/single-product.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container mt-3 align-center" style="min-height:80vh;">
+<div class="container align-center mt-3" style="min-height:80vh;">
+   
     <div class="row div-product ">
         <div class="col-12 col-sm-6 col-product-info">
-            <h1>{{$product->description}}</h1>
+           
+            <h1>{{$product->description}}
+                <a  href="url ">
+                    <img class="editProduct " src="{{ url('img/edit.png') }}" alt="..." >
+                </a>
+            </h1>
             <h2>{{$product->description}}</h2>
             <h3>Talla: {{$product->size}}</h3>
             <h2>Categorias:</h2>
@@ -16,6 +22,7 @@
             <button class="button primary mt-3">Agregar al carrito</button>
         </div>
         <div class="col-12 col-sm-6  col-product-info">
+            
             <img src="{{ url('img/ropa1.png') }}" alt="..." class="col-product-img">
         </div>
 
