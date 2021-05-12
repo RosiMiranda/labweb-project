e448ae272f2e11ff0d4057267a1dfd5ebc24560f
andreabecerrab <andii1wiki@gmail.com>
2021-05-11 20:51:52 -0500

    stripe init config

--- app/User.php
+++ app/User.php
@@ -5,9 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
+    use Billable;
+
     public function role(){
         return $this->hasOne(Role::class);
     }
