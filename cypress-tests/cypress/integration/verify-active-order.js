0b85133c174257077572656e6e461e2bc5829dac
rosmir <36215317+RosiMiranda@users.noreply.github.com>
2021-05-28 14:03:12 -0500

    Active Order Test

---
+++ cypress-tests/cypress/integration/verify-active-order.js
@@ -0,0 +1,48 @@
+var email = 'rosa@email.com';
+var userName = 'Rosa';
+var password = '12345';
+var productDescription = 'Azules, de mezclilla';
+var precio = '500';
+var orderId = '5';
+
+describe('verify-active-order', () => {
+
+    it('VerifyActiveOrder', () => {
+
+        cy.on('uncaught:exception', () => false);
+  
+        cy.visit('http://labweb-project.test/home');
+
+      cy.contains('Iniciar sesión').click({ force: true });
+
+      cy.url().should('include', '/login');
+
+      cy.get('[name="email"]')
+          .type(email);
+      cy.get('[name="password"]')
+          .type(password);
+      cy.get('#login-button').click();
+
+      cy.url().should('include', '/home');
+
+      cy.contains(userName);
+
+      cy.contains(userName).click({ force: true });
+
+      cy.contains('Ordenes Activas').click({ force: true });
+
+      cy.url().should('include', '/order');
+
+      cy.contains(orderId);
+
+      cy.contains('Ver').click({ force: true });
+
+      cy.url().should('include', '/order/'+orderId);
+
+
+      cy.contains(userName);
+      cy.contains(productDescription);
+      cy.contains(precio);
+
+    })
+})
